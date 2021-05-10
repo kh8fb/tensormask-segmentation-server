a9200a24af5e0713e140e5f0dd80ea13567e9641
Kieran Heese <kh8fb@virginia.edu>
2021-05-10 16:50:09 -0400

    Initial commit

---
+++ tensormask_segmentation_server/__init__.py
@@ -0,0 +1,11 @@
+"""
+A server-based method for image segmentation results with the Tensormask model. 
+"""
+
+import click
+
+@click.group()
+def cli_main():
+    """
+    A web based dashboard for obtaining image segmentation modeling results.
+    """
